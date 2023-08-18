95ef640741df973f28339eec1b986fc19770ca4d
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 10:01:19 -0400

    if statements
    In this file you learn how to implement if - else statements in python.

---
+++ 6.if_statements.py
@@ -0,0 +1,16 @@
+# Hello everyone! , we are learning how to write a 'Conditional statements' in python
+
+# Conditional statements allow a program to make decisions based on whether a certain condition is true or false.
+
+first_number = input("The value of first_number is: ")
+second_number = input("The value of second_number is: ")
+
+if first_number > second_number:
+    print("The value of first_number is larger")
+
+if first_number < second_number:
+    print("The value of second_number are larger")
+
+else:
+    print("Both value's are same")
+
