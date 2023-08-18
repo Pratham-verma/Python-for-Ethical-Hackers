c9d88c6c851588685f3adbf3a12f3bb4264831d9
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 09:55:56 -0400

    Variables and comments
    In this files you learn how to write the Variables and comments in python.

---
+++ 2.Variables_and_comment.py
@@ -0,0 +1,12 @@
+# Hello everyone! , we are learning how to write a 'variables' and 'comments' in python
+
+# This is a comment (Basically we use '#' for write a comment )
+# Hello world
+
+# now we write a variables.
+a = 10
+b = 20  # 'a' and 'b' are the variables.
+       
+print(a)
+print(b)
+
