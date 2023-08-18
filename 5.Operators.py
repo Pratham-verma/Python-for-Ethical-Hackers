69789c8d97858a21759c5db31e79a458fff073f0
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 09:59:57 -0400

    Operators
    In this file you learn how to write the operators in python.

---
+++ 5.Operators.py
@@ -0,0 +1,42 @@
+# Hello everyone! , we are learning how to write a 'Operators' in python
+
+# Operators are symbols or keywords in programming that perform specific operations on one or more operands. 
+# Examples :- 
+# 1. Arithmetic operators (+, -, *, /).
+a = 20
+b = 10
+
+print("The sum is:", a+b)
+print("The subtract is:", a-b)
+print("The multiply is:", a*b)
+print("The division is:", a/b)
+
+# 2. Comparison operators (>, <, ==).
+c = 10
+d = 20
+
+print("The value of (c > d) is: ", c>d)
+print("The value of (c < d) is: ", c<d)
+print("The value of (c == d) is: ", c==d)
+
+# 3. Logical operators (and, or, not).
+
+# 1. and Operator:
+# The "and" operator returns 'True' if both operands are True, otherwise, it returns 'False'.
+e = True
+f = False
+
+result = e and f
+print(result)
+
+# 2. or Operator:
+# The "or" operator returns 'True' if at least one of the operands is True, otherwise, it returns 'False'.
+
+result = e or f
+print(result)
+
+# 3. not Operator:
+# The "not" operator returns the opposite of the operand's , If the operand is 'True', not returns False, and if the operand is 'False', not returns True.
+
+result = not f
+print(result)
