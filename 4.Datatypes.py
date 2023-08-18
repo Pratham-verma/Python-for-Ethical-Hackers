b027dc3957ac703f18c4ccbc15c4c8f2f6ad094c
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 09:58:58 -0400

    Datatypes

---
+++ 4.Datatypes.py
@@ -0,0 +1,11 @@
+# Hello everyone! , we are learning how to write a 'Datatypes' in python
+
+# Datatypes in programming are categories that define the type of data a variable can hold.
+# Examples of datatypes include integers,floats,char,strings(text), boolean values(true/false) and more.
+
+a = 12      # integer    
+b = 13.2    # float
+c = 'Hello' # string
+d = False   # boolean(True/False)
+
+print(a,b,c,d)
