30db98f01cdb35f3701e018a48ab84c5e601d24c
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 09:54:43 -0400

    Print
    In this files you learn how to print in python.

---
+++ 1.Print.py
@@ -0,0 +1,6 @@
+# Greetings, everyone!, we are learning the process of printing text in Python.
+# we can print various element like(numbers , words , any sentence) etc.
+
+print('17')
+print('Hello world')
+print('welcome to python for Ethical Hacking course') 
