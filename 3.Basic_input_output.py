27b50ef81db177c3bc4fbbb35613228b4d13210b
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 09:58:18 -0400

    Basic input and output

---
+++ 3.Basic_input_output.py
@@ -0,0 +1,9 @@
+# Hello everyone! , we are learning how to write a 'Basic input/output' in python
+
+# Basic Input/Output refers to the fundamental process of receiving data (input) from users or sending data (output) to displays.
+
+ip = input("what is the target ip address ?\n")
+print("scanning the target: " + ip)
+
+#we can also write like this - 
+print("scanning the target: " + input("what is the target ip address ?\n"))
