ec772c46bacdef701075c33f81ac52aa4a20978c
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 10:21:33 -0400

    While loop

---
+++ 9.While_loop.py
@@ -0,0 +1,17 @@
+# Hello everyone! , we are learning how to implement a 'while loop' in python.
+
+# while loops execute statements while the condition is true. As soon as the condition becomes False.
+
+# simple example of while loop :-
+
+# i = 0
+# while (i<3):
+#    print(i)
+#    i = i+1
+
+# another example :-
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
