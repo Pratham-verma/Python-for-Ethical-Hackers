7977e2414e80c8e11c057c47bf68f14bebc7b780
Pratham verma <89769653+Pratham-verma@users.noreply.github.com>
2023-08-18 10:21:04 -0400

    For loop

---
+++ 8.For_loop.py
@@ -0,0 +1,12 @@
+# Hello everyone! , we are learning how to implement a 'For loop' in python.
+
+# A "for loop" is like a repetitive task manager in programming. It helps you do the same thing multiple times, like counting or performing an action on a bunch of items.
+
+# we take a simple example to understand :-
+# find a number which is divisible by 2 between 0 and 20 ?
+
+for num in range(1,20,):  
+   if num % 2 == 0:
+     print(num)
+
+
